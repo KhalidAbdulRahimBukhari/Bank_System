e4781f5675b71c5692442aefa259057c69cbf708
khalid <k.bukhari.1418@gmail.com>
2025-10-27 20:09:48 +0100

    Updated SQL file (Add get by ID SPs)

--- Database File/Create_SPs.sql
+++ Database File/Create_SPs.sql
@@ -5,6 +5,9 @@
 
 --USE [MyBankSystem]
 
+---------------------------------------------------------------------------
+--								GET All 
+---------------------------------------------------------------------------
 --create procedure SP_GetAllUsers
 --AS
 --begin
@@ -24,5 +27,51 @@
 --begin
 --	select * from Transaction_View
 --end
+--------------------------------------------------------------------------------------
 
+
+---------------------------------------------------------------------------
+--							GET Client, User Or Transaction 
+---------------------------------------------------------------------------
+--create procedure [dbo].[SP_GetClientByID]
+--			@Client_ID INT
+--AS
+--begin
+--	select * from ClientPersonView where Client_ID = @Client_ID
+--end
+
+--Create procedure SP_GetClientByAccountNumber
+--			@AccountNumber NVARCHAR(50)
+--AS
+--begin
+--	select * from ClientPersonView where AccountNumber = @AccountNumber
+--end
+
+
+--Create procedure SP_GetUserByID
+--			@User_ID INT
+--AS
+--begin
+--	select * from UserPersonView where User_ID = @User_ID
+--end
+
+--Create procedure SP_GetUserByUserName
+--			@UserName Nvarchar(50)
+--AS
+--begin
+--	select * from UserPersonView where UserName = @UserName
+--end
+
+
+--Create procedure SP_GetTransactionByID
+--			@Transaction_ID INT
+--AS
+--begin
+--	select * from Transactions where Transaction_ID = @Transaction_ID
+--end
+
+
+---------------------------------------------------------------------------
+--							Update Client OR User  
+---------------------------------------------------------------------------
 
